4268287e8fd555b7f78a0752c170a5e2298b7c75
Astha Thakur <128837575+aasthathakor@users.noreply.github.com>
2024-03-21 10:23:53 +0530

    first commit

---
+++ src/app/signin/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignIn from '../../components/signin/signin'
+
+
+const page = () => {
+  return (
+    <>
+      <SignIn /> 
+    </>
+  )
+}
+
+export default page
